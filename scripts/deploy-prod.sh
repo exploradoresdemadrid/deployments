edb43ecb25d13382dd679f5e55fc24a55911cf7e
Manuel Bustillo <mayn13@gmail.com>
2020-06-13 17:04:21 +0200

    Prune old docker images before deploying

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker image prune -af
+
 source ../secrets/prod-secrets.sh
 
 docker-compose -f ../compose/docker-compose-prod.yml pull && docker-compose -f ../compose/docker-compose-prod.yml up -d
