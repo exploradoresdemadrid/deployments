7ecfe2eca4bc1c58917c45f594b4715fb13852e9
Manuel <manuel.hna@gmail.com>
2021-01-31 17:02:34 +0100

    Backup script cleanup
    Introduced for loop with database names which can be expanded if needed.
    Also cleaned script up for better readability.

--- scripts/backup-upload.sh
+++ scripts/backup-upload.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-## source environmient variables
+## source environmient variables for later use
 source /home/ubuntu/deployments/secrets/prod-secrets.sh
 
 DECIDE_PROD_NAME=decide_production_`date +%Y-%m-%d"_"%H_%M_%S`
 SORTING_HAT_PRODUCTION=sorting-hat_production_`date +%Y-%m-%d"_"%H_%M_%S`
 BACKUPS_DIR=/home/ubuntu/backups
 
-docker exec -t compose_db_1 pg_dumpall -c -U decide_prod -l decide_production \
-> $BACKUPS_DIR/$DECIDE_PROD_NAME.sql
-
-tar -czvf $BACKUPS_DIR/$DECIDE_PROD_NAME.tar.gz $BACKUPS_DIR/$DECIDE_PROD_NAME.sql
-
-docker exec -t compose_db_1 pg_dumpall -c -U hat_production -l decide_production \
-> $BACKUPS_DIR/$SORTING_HAT_PRODUCTION.sql
-
-tar -czvf $BACKUPS_DIR/$SORTING_HAT_PRODUCTION.tar.gz $BACKUPS_DIR/$SORTING_HAT_PRODUCTION.sql
+declare -a databases=(decide_production sorting-hat_production)
 
 ## Get new access token with refresh token
 GCLOUD_ACCESS_TOKEN=""
@@ -27,22 +19,26 @@ client_id=$GCLOUD_CLIENT_ID \
 https://accounts.google.com/o/oauth2/token | jq '.access_token')
 
 
-# Upload file to folder
-curl \
--X POST -L \
--H "Authorization: Bearer $GCLOUD_ACCESS_TOKEN" \
--F "metadata={name :'$DECIDE_PROD_NAME.tar.gz', parents :['$GCLOUD_FOLDER_ID']};type=application/json;charset=UTF-8" \
--F "file=@$BACKUPS_DIR/$DECIDE_PROD_NAME.tar.gz;type=application/gzip" \
-"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
 
+for database in ${databases[@]}; do
+    # Backup name with db name + timestamp
+    local backup_name=$database-`date +%Y-%m-%d"_"%H_%M_%S`
 
-curl \
--X POST -L \
--H "Authorization: Bearer $GCLOUD_ACCESS_TOKEN" \
--F "metadata={name :'$SORTING_HAT_PRODUCTION.tar.gz', parents :['$GCLOUD_FOLDER_ID']};type=application/json;charset=UTF-8" \
--F "file=@$BACKUPS_DIR/$SORTING_HAT_PRODUCTION.tar.gz;type=application/gzip" \
-"https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+    # Docker execution of postgres backup
+    docker exec -t\
+    compose_db_1 pg_dumpall -c -U decide_prod -l $database > $BACKUPS_DIR/$backup_name.sql
 
+    # Compresion of db backup  
+    tar -czvf $BACKUPS_DIR/$backup_name.tar.gz $BACKUPS_DIR/$backup_name.sql
+    
+    # Upload of compressed backup to gdrive with token
+    curl \
+    -X POST -L \
+    -H "Authorization: Bearer $GCLOUD_ACCESS_TOKEN" \
+    -F "metadata={name :'$backup_name.tar.gz', parents :['$GCLOUD_FOLDER_ID']};type=application/json;charset=UTF-8" \
+    -F "file=@$BACKUPS_DIR/$backup_name.tar.gz;type=application/gzip" \
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+done
 
 ## Cleanup after backups
 rm -v $BACKUPS_DIR/*.tar.gz
