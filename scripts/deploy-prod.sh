552f059e450065ea80562955402949f9a21a104e
Manuel Bustillo <mayn13@gmail.com>
2021-03-29 00:45:43 +0200

    Stop local DNS resolver before starting DNS server

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,6 +3,13 @@
 cd ..
 source secrets/prod-secrets.sh
 
-docker-compose -f compose/docker-compose.yml pull && docker-compose -f compose/docker-compose.yml up -d
+# Download docker images
+docker-compose -f compose/docker-compose.yml pull
+
+# Stop local DNS server
+sudo systemctl stop systemd-resolved
+
+# Start docker images (including DNS server)
+docker-compose -f compose/docker-compose.yml up -d
 
 docker image prune -af
