d064b7c25e8b9160ce3982716f5a7f95df1ab13d
Manuel Bustillo <mayn13@gmail.com>
2021-04-01 12:00:55 +0200

    Remove webmin installation

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,17 +3,6 @@
 cd ..
 source secrets/prod-secrets.sh
 
-# Start local DNS server
-docker stop compose_bind_1
-sudo systemctl start systemd-resolved
-
-# Download docker images
-docker-compose -f compose/docker-compose.yml pull
-
-# Stop local DNS server
-sudo systemctl stop systemd-resolved
-
-# Start docker images (including DNS server)
-docker-compose -f compose/docker-compose.yml up -d
+docker-compose -f compose/docker-compose.yml pull && docker-compose -f compose/docker-compose.yml up -d
 
 docker image prune -af
