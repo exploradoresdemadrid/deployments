316dbcc52965ba4c42c74caab796107ef967cb5d
Manuel Bustillo <manuel.bustillo@coverwallet.com>
2021-05-02 13:12:35 +0200

    Configure file backups in Salamandra with rclone

--- scripts/backup-upload.sh
+++ scripts/backup-upload.sh
@@ -52,6 +52,8 @@ curl \
     -F "file=@$backup_name.tar.gz;type=application/gzip" \
     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
 
+## Configure file backup with rclone
+rclone copy --immutable /home/ubuntu/salamandra/private_files tecnologia_edm:salamandra/private_files
 
 ## Cleanup after backups
 rm -v ./*.tar.gz
