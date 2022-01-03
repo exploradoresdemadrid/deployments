a9f19e1042b598485bdc3afa06ae4c3d7721cef6
Manuel Bustillo <mayn13@gmail.com>
2022-01-03 08:34:59 +0100

    Restart nginx after deployment

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,6 +3,6 @@
 cd ..
 source secrets/prod-secrets.sh
 
-docker-compose -f compose/docker-compose.yml pull && docker-compose -f compose/docker-compose.yml up -d
+docker-compose -f compose/docker-compose.yml pull && docker-compose -f compose/docker-compose.yml up -d && docker-restart compose_nginx_1
 
 docker image prune -af
